a133240ec73c36067e4bb8c27dc455157e382930
838902701@qq.com <838902701@qq.com>
2019-07-17 23:46:37 +0800

    Test

--- Every-day/src/main/java/add.java
+++ Every-day/src/main/java/add.java
@@ -1,15 +1,49 @@
-//public class add {
-//    /***
-//     * 两个超长整数相加
-//     * String addend
-//     * String augend
-////     *
-////     */
-////    public static void main(String[] args) {
-////
-////    }
-////    public String AddLongInteger(String addend, String augend) {
-////
-////
-////    }
-////}
+import java.util.*;
+import java.util.*;
+
+public class Replacement {
+    public String replaceSpace(String iniString, int length) {
+        // write code here
+        if (iniString == null || iniString.length() == 0) {
+            return null;
+        }
+
+        char[] str = iniString.toCharArray();
+
+
+        int originalLength = 0;
+        int numberOfBlank = 0;
+
+        int i = 0;
+        while (str[i] != '\0') ;
+        {
+            ++originalLength;
+            if (str[i] == ' ') {
+                ++numberOfBlank;
+            }
+            ++i;
+        }
+
+
+        int newLength = originalLength + numberOfBlank * 2;
+
+        if (newLength > length) {
+            return null;
+        }
+
+        int indexOfOriginal = originalLength;
+        int indexOfNew = newLength;
+        while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal) {
+            if (str[indexOfOriginal] == ' ') {
+                str[indexOfNew--] = '0';
+                str[indexOfNew--] = '2';
+                str[indexOfNew--] = '%';
+            } else {
+                str[indexOfNew--] = str[indexOfOriginal];
+            }
+            --indexOfOriginal;
+
+
+        }
+    }
+}
---
+++ Every-day/src/main/java/day9/Root.java
@@ -0,0 +1,62 @@
+package day9;
+
+/***
+ * 有一棵无穷大的满二叉树，其结点按根结点一层一层地从左往右依次编号，根结点编号为1。现在有两个结点 a，b。
+ * 请设计一个算法，求出a和b点的最近公共祖先的编号。 给定两个int a,b。为给定结点的编号。
+ */
+public class Root {
+
+
+
+    public static void main(String[] args) {
+
+    }
+    public String replaceSpace(String iniString, int length) {
+        // write code here
+        if(iniString == null || iniString.length() ==0) {
+            return null;
+        }
+
+        char[] str = iniString.toCharArray();
+
+
+        int originalLength = 0;
+        int numberOfBlank = 0;
+
+        int i =0;
+        while (str[i] != '\0'); {
+            ++originalLength;
+            if(str[i] == ' ') {
+                ++numberOfBlank;
+            }
+            ++i;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    private static int find(int left, int right) {
+        while (left != right) {
+            if(left > right) {
+                left = left / 2;
+            } else {
+                right = right / 2;
+            }
+        }
+        return left;
+    }
+}
